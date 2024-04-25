3dfef09cbb727ddbea36b35d0775fc0c02b59fb4
Stepan <stepstepan2@gmail.com>
2024-04-26 02:02:15 +0300

    all finished

--- processing.py
+++ processing.py
@@ -1,21 +1,25 @@
 from datetime import datetime
 
 
-def sorting_dict_status(user_dictionary: list, status: str = 'EXECUTED') -> list:
-    '''Сортирует список по status '''
+def sorting_dict_status(user_dictionary: list, status: str = "EXECUTED") -> list:
+    """Сортирует список по status"""
     list_correct = []
     for i in user_dictionary:
         for k in i.keys():
-            if status == 'EXECUTED':
-                if i[k] == 'EXECUTED':
+            if status == "EXECUTED":
+                if i[k] == "EXECUTED":
                     list_correct.append(i)
             else:
-                if i[k] == 'CANCELED':
+                if i[k] == "CANCELED":
                     list_correct.append(i)
     return list_correct
 
 
 def sorting_dict_reverse(user_time: list, flag: bool = False) -> list:
-    '''сортирует по убыванию, возрастанию'''
-    sorted(user_time, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=flag)
+    """сортирует по убыванию, возрастанию"""
+    sorted(
+        user_time,
+        key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"),
+        reverse=flag,
+    )
     return user_time
